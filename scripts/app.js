45e3b43c48f7afffaeecc11fa6a12330ec706204
TemesgeA <temesgen.ayneka@gmail.com>
2022-07-07 08:04:53 +0300

    feature to add book to local storage

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,21 @@
-const booksList = document.querySelectorAll("#results");
-new URLSearchParams(window.location.search).forEach((value, name) => {
-  booksList.append(`${name}, ${value}`);
-  booksList.append(document.createElement("br"));
-});
+// Initialize variables to add books to localStorage
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const addBookBtn = document.querySelector('.add-book');
+
+// Event to add a book to local storage
+addBookBtn.addEventListener("click", addBook);
+function addBook() {
+  if (!bookTitle.value || !bookAuthor.value) return;
+  const books = JSON.parse(localStorage.getItem("books")) || [];
+  const book = {};
+  book.title = bookTitle.value;
+  book.author = bookAuthor.value;
+  book.index = books.length;
+  books.push(book);
+  
+  localStorage.setItem("books", JSON.stringify(books));
+  bookTitle.value = "";
+  bookAuthor.value = "";
+  displayBooks();
+}
