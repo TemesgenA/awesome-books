8fcdef335d8c70a2aad3d81ede27868502f2eaf7
TemesgeA <temesgen.ayneka@gmail.com>
2022-07-08 14:24:09 +0300

    fix bug in display ui to DOM connection

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,6 @@
 // Initialize variables to add books to local Storage
-const bookTitle = document.querySelector('#title');
-const bookAuthor = document.querySelector('#author');
-const addBookBtn = document.querySelector('.add-book');
-
-// Initilise books list
-const booksList = document.querySelector('.books-list');
+const addBookBtn = document.querySelector(".add-book");
+const booksList = document.querySelector(".books-list");
 
 // Class to Represent a Book
 class Book {
@@ -26,30 +22,30 @@ class UI {
 
   static addBookToList(book) {
     // create book Div
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book");
 
     // Create title
-    const titleElement = document.createElement('h3');
+    const titleElement = document.createElement("h3");
     titleElement.innerText = book.title;
 
     // create Author
-    const authorElement = document.createElement('h3');
+    const authorElement = document.createElement("h3");
     authorElement.innerText = book.author;
 
     // Create Remove Btn
-    const removeElement = document.createElement('button');
-    removeElement.classList.add('remove-btn');
-    removeElement.innerText = 'Remove';
-    removeElement.setAttribute('data-id', book.index);
+    const removeElement = document.createElement("button");
+    removeElement.classList.add("remove-btn");
+    removeElement.innerText = "Remove";
+    removeElement.setAttribute("data-id", book.index);
 
     // Create infoDiv element
-    const infoDiv = document.createElement('div');
-    infoDiv.classList.add('info');
+    const infoDiv = document.createElement("div");
+    infoDiv.classList.add("info");
 
     // create h3
-    const linkElement = document.createElement('h3');
-    linkElement.innerText = 'by';
+    const linkElement = document.createElement("h3");
+    linkElement.innerText = "by";
 
     infoDiv.appendChild(titleElement);
     infoDiv.appendChild(linkElement);
@@ -61,12 +57,12 @@ class UI {
   }
 
   static clearFields() {
-    document.getElementById('title-input').value = '';
-    document.getElementById('author-input').value = '';
+    document.getElementById("bookTitle").value = "";
+    document.getElementById("bookAuthor").value = "";
   }
 
   static deleteBook(el) {
-    if (el.classList.contains('remove-btn')) {
+    if (el.classList.contains("remove-btn")) {
       el.parentElement.remove();
     }
   }
@@ -91,7 +87,7 @@ class Store {
     localStorage.setItem("books", JSON.stringify(books));
   }
 
-  static removeBook(isbn) {
+  static removeBook(title) {
     const books = Store.getBooks();
 
     books.forEach((book, index) => {
@@ -104,48 +100,23 @@ class Store {
   }
 }
 
-// Event: Display Books
+// Event to display books
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
-// Event: Add a Book
-document.querySelector("#book-form").addEventListener("submit", (e) => {
-  // Prevent actual submit
-  e.preventDefault();
-
-  // Get form values
-  const title = document.querySelector("#title").value;
-  const author = document.querySelector("#author").value;
-  const isbn = document.querySelector("#isbn").value;
-
-  // Validate
-  if (title === "" || author === "" || isbn === "") {
-    UI.showAlert("Please fill in all fields", "danger");
-  } else {
-    // Instatiate book
-    const book = new Book(title, author, isbn);
-
-    // Add Book to UI
+// Event to add books
+addBtn.addEventListener('click', () => {
+  const bookTitle = document.querySelector('#title').value;
+  const bookAuthor = document.querySelector('#author').value;
+  if (bookTitle && bookAuthor) {
+    const book = new Book(bookTitle, bookAuthor);
     UI.addBookToList(book);
-
-    // Add book to store
     Store.addBook(book);
-
-    // Show success message
-    UI.showAlert("Book Added", "success");
-
-    // Clear fields
     UI.clearFields();
   }
 });
-
-// Event: Remove a Book
-document.querySelector("#book-list").addEventListener("click", (e) => {
-  // Remove book from UI
+// Event to remove books
+booksList.addEventListener('click', (e) => {
   UI.deleteBook(e.target);
-
-  // Remove book from store
-  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
-
-  // Show success message
-  UI.showAlert("Book Removed", "success");
+  console.log(e.target.previousElementSibling.children[2]);
+  Store.removeBook(e.target.previousElementSibling.children[2].textContent);
 });
