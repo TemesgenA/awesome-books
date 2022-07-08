f05e8dbbca85920335cd3492057eb5f200923b27
TemesgeA <temesgen.ayneka@gmail.com>
2022-07-08 12:11:00 +0300

    add Class to handle local storage

--- scripts/app.js
+++ scripts/app.js
@@ -72,27 +72,37 @@ class UI {
   }
 }
 
-// Event to display books
-window.addEventListener('DOMContentLoaded', displayBooks);
+// Class to handle local storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
 
-// Event to add a book to local storage
-function addBook() {
-  if (!bookTitle.value || !bookAuthor.value) return;
-  const books = JSON.parse(localStorage.getItem('books')) || [];
-  const book = {};
-  book.title = bookTitle.value;
-  book.author = bookAuthor.value;
-  book.index = books.length;
-  books.push(book);
+    return books;
+  }
 
-  localStorage.setItem('books', JSON.stringify(books));
-  bookTitle.value = '';
-  bookAuthor.value = '';
-  displayBooks();
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
 }
-
-// Event to add books
-addBookBtn.addEventListener('click', addBook);
 
 // Function to remove a book
 function removeBook(e) {
