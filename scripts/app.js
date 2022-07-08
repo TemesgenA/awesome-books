1f356d787cb0bc65898c0d2171bac394a631bb47
TemesgeA <temesgen.ayneka@gmail.com>
2022-07-08 12:13:44 +0300

    add event listeners to add, remove and display books on UI

--- scripts/app.js
+++ scripts/app.js
@@ -104,17 +104,48 @@ class Store {
   }
 }
 
-// Function to remove a book
-function removeBook(e) {
-  const item = e.target;
-  if (item.classList[0] === 'remove-btn') {
-    const books = JSON.parse(localStorage.getItem('books'));
-    const indexInd = Number(item.dataset.id);
-    const modified = books.filter((book) => book.index !== indexInd);
-    localStorage.setItem('books', JSON.stringify(modified));
-  }
-  displayBooks();
-}
+// Event: Display Books
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
 
-// Event to remove a book
-booksList.addEventListener('click', removeBook);
+// Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    // Instatiate book
+    const book = new Book(title, author, isbn);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+
+    // Add book to store
+    Store.addBook(book);
+
+    // Show success message
+    UI.showAlert("Book Added", "success");
+
+    // Clear fields
+    UI.clearFields();
+  }
+});
+
+// Event: Remove a Book
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  // Remove book from UI
+  UI.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+  // Show success message
+  UI.showAlert("Book Removed", "success");
+});
