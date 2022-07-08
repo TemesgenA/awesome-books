2655fc74d00b10b64422c4d19ba4bb11d8678eaf
TemesgeA <temesgen.ayneka@gmail.com>
2022-07-08 21:37:06 +0300

    add JS listeners

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,12 @@
 // Initialize variables to add books to local Storage
 const addBookBtn = document.querySelector('.add-book-btn');
 const booksList = document.querySelector('.books-list');
+const allBooksSection = document.querySelector('#all-books-section');
+const addBookSection = document.querySelector('#books-section');
+const contactSection = document.querySelector('#contact-section');
+const listNavLink = document.querySelector('.list-nav-link');
+const addNavLink = document.querySelector('.add-nav-link');
+const contactNavLink = document.querySelector('.contact-nav-link');
 
 // Class to Represent a Book
 class Book {
@@ -107,4 +113,35 @@ addBookBtn.addEventListener('click', () => {
 booksList.addEventListener('click', (e) => {
   UI.deleteBook(e.target);
   Store.removeBook(e.target.previousElementSibling.children[2].textContent);
+});
+
+listNavLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  allBooksSection.classList.add('show-section');
+  allBooksSection.classList.remove('hide-section');
+  addBookSection.classList.add('hide-section');
+  contactSection.classList.add('hide-section');
+  listNavLink.children[0].classList.add('active');
+  contactNavLink.children[0].classList.remove('active');
+  addNavLink.children[0].classList.remove('active');
+});
+addNavLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  addBookSection.classList.add('show-section');
+  addBookSection.classList.remove('hide-section');
+  contactSection.classList.add('hide-section');
+  allBooksSection.classList.add('hide-section');
+  addNavLink.children[0].classList.add('active');
+  listNavLink.children[0].classList.remove('active');
+  contactNavLink.children[0].classList.remove('active');
+});
+contactNavLink.addEventListener('click', (e) => {
+  e.preventDefault();
+  contactSection.classList.add('show-flex-section');
+  contactSection.classList.remove('hide-section');
+  addBookSection.classList.add('hide-section');
+  allBooksSection.classList.add('hide-section');
+  contactNavLink.children[0].classList.add('active');
+  listNavLink.children[0].classList.remove('active');
+  addNavLink.children[0].classList.remove('active');
 });
